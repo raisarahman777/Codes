ac3ca732aea8b5d73f69478157f176738ca19439
raisarahman777 <102578592+raisarahman777@users.noreply.github.com>
2023-11-09 08:57:11 +0600

    Create LIS.cpp

---
+++ LIS.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define lili long long int
+using namespace std;
+
+int lengthOfLIS(vector<int>& arr) {
+	int n = arr.size();
+	int lis_len_ending_here[n + 1];
+	int lis_len = 1;
+	for(int i = 0; i < n; i++) {
+		lis_len_ending_here[i] = 1;
+	}
+	for(int i = 1; i < n; i++) {
+		for(int j = 0; j < i; j++) {
+			if(arr[j] < arr[i] && lis_len_ending_here[j] + 1 > lis_len_ending_here[i]) {
+				lis_len_ending_here[i] = lis_len_ending_here[j] + 1;
+			}
+		}
+		lis_len = max(lis_len, lis_len_ending_here[i]);
+	}
+	return lis_len;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(auto& it : a) {
+		cin >> it;
+	}
+	int lis_len = lengthOfLIS(a);
+	cout << lis_len << "\n";
+    return 0;
+}
+
+/*
+8
+-7 10 9 2 3 8 8 6
+
+Output: 4
+*/
