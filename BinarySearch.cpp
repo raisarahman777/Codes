c39a0c06239c6bfe2f114782b0d535241581a1e4
raisarahman777 <102578592+raisarahman777@users.noreply.github.com>
2023-12-15 03:05:42 +0600

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+using namespace std;
+
+int n = 14;
+int arr[] = {5, 9, 10, 10, 10, 14, 17, 17, 20, 22, 22, 22, 22, 29};
+
+int first_index(int first, int last, int x) {
+    int lo = first, hi = last, mid;
+    while(lo < hi) {
+        mid = (lo + hi) / 2;
+        if(arr[mid] < x) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+    if(arr[lo] == x) {
+        return lo;
+    }
+    return -1;
+}
+
+// int first_index(int first, int last, int x) {
+//     int lo = first, hi = last, mid, ans = -1;
+//     while(lo <= hi) {
+//         mid = (lo + hi) / 2;
+//         if(arr[mid] == x) {
+//             ans = mid;
+//         }
+//         if(arr[mid] < x) {
+//             lo = mid + 1;
+//         }
+//         else {
+//             hi = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
+
+int last_index(int first, int last, int x) {
+    int lo = first, hi = last, mid;
+    while(lo < hi) {
+        mid = (lo + hi + 1) / 2;
+        if(arr[mid] > x) {
+            hi = mid - 1;
+        }
+        else {
+            lo = mid;
+        }
+    }
+    if(arr[lo] == x) {
+        return lo;
+    }
+    return -1;
+}
+
+// int last_index(int first, int last, int x) {
+//     int lo = first, hi = last, mid, ans = -1;
+//     while(lo <= hi) {
+//         mid = (lo + hi) / 2;
+//         if(arr[mid] == x) {
+//             ans = mid;
+//         }
+//         if(arr[mid] > x) {
+//             hi = mid - 1;
+//         }
+//         else {
+//             lo = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+
+int greater_than(int first, int last, int x) {
+    if(arr[last] <= x) {
+        return -1;
+    }
+    int lo = first, hi = last, mid;
+    while(lo < hi) {
+        mid = (lo + hi) / 2;
+        if(arr[mid] <= x) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+    return lo;
+}
+
+// int greater_than(int first, int last, int x) {
+//     int lo = first, hi = last, mid, ans = -1;
+//     while(lo <= hi) {
+//         mid = (lo + hi) / 2;
+//         if(arr[mid] <= x) {
+//             lo = mid + 1;
+//         }
+//         else {
+//             hi = mid - 1;
+//             ans = mid;
+//         }
+//     }
+//     return ans;
+// }
+
+int greater_than_or_equal(int first, int last, int x) {
+    if(arr[last] < x) {
+        return -1;
+    }
+    int lo = first, hi = last, mid;
+    while(lo < hi) {
+        mid = (lo + hi) / 2;
+        if(arr[mid] < x) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+    return lo;
+}
+
+// int greater_than_or_equal(int first, int last, int x) {
+//     int lo = first, hi = last, mid, ans = -1;
+//     while(lo <= hi) {
+//         mid = (lo + hi) / 2;
+//         if(arr[mid] < x) {
+//             lo = mid + 1;
+//         }
+//         else {
+//             hi = mid - 1;
+//             ans = mid;
+//         }
+//     }
+//     return ans;
+// }
+
+int main() {
+    int x;
+    while(cin >> x) {
+        cout << greater_than_or_equal(0, n - 1, x) << endl;
+    }
+}
