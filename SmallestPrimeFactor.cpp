b2d78f92a8c62fa3a14188ec266231184b9a13e1
raisarahman777 <102578592+raisarahman777@users.noreply.github.com>
2024-02-10 22:47:04 +0600

    Update SmallestPrimeFactor.cpp

--- SmallestPrimeFactor.cpp
+++ SmallestPrimeFactor.cpp
@@ -6,13 +6,13 @@ const int N = 1e6 + 2;
 int spf[N];
 
 void sieve() {
-	for(int i = 2; i < N; i++) {
-		if(i % 2) spf[i] = i;
+	for (int i = 2; i < N; i++) {
+		if (i % 2) spf[i] = i;
 		else spf[i] = 2;
 	}
-	for(int i = 3; (lili)i * i < N; i+= 2) {
-		if(spf[i] == i) {
-			for(lili j = i * i; j < N; j += i) {
+	for (int i = 3; (lili)i * i < N; i+= 2) {
+		if (spf[i] == i) {
+			for (lili j = i * i; j < N; j += i) {
 				spf[j] = min(spf[j], i);
 			}
 		}
@@ -23,7 +23,7 @@ int main() {
 	ios_base::sync_with_stdio(false);
   	cin.tie(NULL);
 	sieve();
-	for(int i = 2; i <= 100; i++) {
+	for (int i = 2; i <= 100; i++) {
 		cout << i << " " << spf[i] << endl;
 	}
 }
